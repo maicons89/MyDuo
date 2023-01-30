8c44763ea0f73f1af05825bff2cd46405a8d353b
maicons89 <maicon.santos89@hotmail.com>
2023-01-29 21:31:40 -0300

    Terminando o formulario e adicionando o botao

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,14 +1,12 @@
-import './CampoTexto.css'
+import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
-    
-    return (
-        <div className="campo-texto">
-            <label>{props.label}</label>
-            <input placeholder={props.placeholder}/>
-        </div>
-    )
+  return (
+    <div className="campo-texto">
+      <label>{props.label}</label>
+      <input required={props.obrigatorio} placeholder={props.placeholder} />
+    </div>
+  );
+};
 
-}
-
-export default CampoTexto
+export default CampoTexto;
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,19 +1,55 @@
 import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
+import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 
 const Formulario = () => {
+  const times = [
+    "COD Warzone",
+    "Owerwatch",
+    "Apex Legends",
+    "Fortnite",
+    "Mario Kart 8",
+    "Splatoon 3",
+  ];
+
+  const aoSalvar = (evento) => {
+    evento.preventDefault();
+    console.log("form foi submetido");
+  };
+
   return (
     <section className="formulario">
-      <form>
+      <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do player. </h2>
-        <CampoTexto label="Nome" placeholder="Digite o seu nome" />
-        <CampoTexto label="Nick" placeholder="Digite o seu nick" />
-        <CampoTexto label="Console" placeholder="Digite o seu console" />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite o seu nome"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Nick"
+          placeholder="Digite o seu nick"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Console"
+          placeholder="Digite o seu console"
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Jogo"
+          placeholder="Digite o jogo que você está online"
+        />
+
         <CampoTexto
           label="Imagem"
           placeholder="Digite o seu endereço da imagem"
         />
-      </form>{" "}
+        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <Botao texto="Criar Card" />
+      </form>
     </section>
   );
 };
