299233989cfe5d791b6990b9848f97a4a3b9fa8f
maicons89 <maicon.santos89@hotmail.com>
2023-02-04 20:28:03 -0300

    Adicionando lista de jogos e criando os times

--- src/App.js
+++ src/App.js
@@ -1,11 +1,63 @@
+import { useState } from "react";
 import Banner from "./componentes/Banner/Banner";
 import Formulario from "./componentes/Formulario";
+import Time from "./componentes/Time";
 
 function App() {
+  const times = [
+    {
+      nome: "COD Warzone",
+      corPrimaria: "#57C278",
+      corSecundaria: "#D9F7E9",
+    },
+    {
+      nome: "Apex Legends",
+      corPrimaria: "#82CFFA",
+      corSecundaria: "#E8F8FF",
+    },
+    {
+      nome: "Mario Kart 8",
+      corPrimaria: "#A6D157",
+      corSecundaria: "#F0F8E2",
+    },
+    {
+      nome: "Overwatch",
+      corPrimaria: "#E06B69",
+      corSecundaria: "#FDE7E8",
+    },
+    {
+      nome: "Fortnite",
+      corPrimaria: "#DB6EBF",
+      corSecundaria: "#FAE9F5",
+    },
+    {
+      nome: "Splatoon 3",
+      corPrimaria: "#FFBA05",
+      corSecundaria: "#FFF5D9",
+    },
+  ];
+
+  const [jogadores, setJogadores] = useState([]);
+
+  const aoNovoJogadorAdicionado = (jogador) => {
+    console.log(jogador);
+    setJogadores([...jogadores, jogador]);
+  };
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario
+        aoJogadorCadastrado={(jogador) => aoNovoJogadorAdicionado(jogador)}
+      />
+
+      {times.map((time) => (
+        <Time
+          key={time.nome}
+          nome={time.nome}
+          corPrimaria={time.corPrimaria}
+          corSecundaria={time.corSecundaria}
+        />
+      ))}
     </div>
   );
 }
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -2,20 +2,35 @@ import "./Formulario.css";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
+import { useState } from "react";
 
 const Formulario = () => {
   const times = [
     "COD Warzone",
-    "Owerwatch",
     "Apex Legends",
-    "Fortnite",
     "Mario Kart 8",
+    "Overwatch",
+    "Fortnite",
     "Splatoon 3",
   ];
 
+  const [nome, setNome] = useState("");
+  const [nick, setNick] = useState("");
+  const [console, setConsole] = useState("");
+  const [jogo, setJogo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
+
   const aoSalvar = (evento) => {
     evento.preventDefault();
-    console.log("form foi submetido");
+    this.props.first.aoJogadorCadastrado({
+      nome,
+      nick,
+      console,
+      jogo,
+      imagem,
+      time,
+    });
   };
 
   return (
@@ -26,28 +41,44 @@ const Formulario = () => {
           obrigatorio={true}
           label="Nome"
           placeholder="Digite o seu nome"
+          valor={nome}
+          aoAlterado={(valor) => setNome(valor)}
         />
         <CampoTexto
           obrigatorio={true}
           label="Nick"
           placeholder="Digite o seu nick"
+          valor={nick}
+          aoAlterado={(valor) => setNick(valor)}
         />
         <CampoTexto
           obrigatorio={true}
           label="Console"
           placeholder="Digite o seu console"
+          valor={console}
+          aoAlterado={(valor) => setConsole(valor)}
         />
         <CampoTexto
           obrigatorio={true}
           label="Jogo"
           placeholder="Digite o jogo que você está online"
+          valor={jogo}
+          aoAlterado={(valor) => setJogo(valor)}
         />
 
         <CampoTexto
           label="Imagem"
           placeholder="Digite o seu endereço da imagem"
+          valor={imagem}
+          aoAlterado={(valor) => setImagem(valor)}
         />
-        <ListaSuspensa obrigatorio={true} label="Time" itens={times} />
+        <ListaSuspensa
+          obrigatorio={true}
+          label="Time"
+          itens={times}
+          valor={time}
+          aoAlterado={(valor) => setTime(valor)}
+        />
         <Botao texto="Criar Card" />
       </form>
     </section>
